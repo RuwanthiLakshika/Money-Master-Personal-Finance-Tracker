01d49f784c2d115bac3370377998ab9479ca82ec
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-06-06 22:13:58 +0530

    create initial setup in dashboard

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,7 +1,14 @@
+import { useUser } from "@clerk/clerk-react";
+
+
 export const Dashboard = () => {
+    const {user}=useUser();
     return (
-        <div>
-        <h1>Dashboard</h1>
+        <>
+        <div className="dashboard-container">
+            <h1>Welcome {user?.firstName}! Here Are Your Finances:</h1>
+
         </div>
+        </>
     )
-    };
+};
