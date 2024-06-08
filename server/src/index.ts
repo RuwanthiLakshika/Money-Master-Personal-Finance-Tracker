cc1031590dcd6b1a27d09cc7e6967fb865d1441e
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-06-08 11:19:49 +0530

    create mongodb database

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+
+const app: Express = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+const mongoURI: string = 
+    "mongodb+srv://runthilakshika:fZUOYdZANyDCqOoC@financetrackerapp.xaim0tk.mongodb.net/";
+
+mongoose.connect(mongoURI)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Failed to connect to MongoDB:", err));
